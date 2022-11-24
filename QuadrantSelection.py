0909352ec80ab59c3584829d98330d297570cef1
ETALATE <84042296+ETALATE@users.noreply.github.com>
2022-11-24 16:26:14 +0100

    ready for Kattis

---
+++ QuadrantSelection.py
@@ -0,0 +1,14 @@
+# Your job is to take a point and determine the quadrant it is in. You can assume that neither of the two coordinates will be .
+# The first line of input contains the integer x. The second line of input contains the integer y.
+
+x_coordiante = int(input())
+y_coordiante = int(input())
+
+if x_coordiante > 0 and y_coordiante > 0:
+    print(1)
+elif x_coordiante > 0 and y_coordiante < 0:
+    print(4)
+elif x_coordiante < 0 and y_coordiante < 0:
+    print(3)
+else: 
+    print(2)
